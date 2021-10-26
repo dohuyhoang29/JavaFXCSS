b7f8e231318ffdd4fd06089d42c4e7b068fea1cd
khang <khang.pd.873@aptechlearning.edu.vn>
2021-10-26 11:42:14 +0700

    test git may khang ngu

--- src/com/Main.java
+++ src/com/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("view/simpleUI.fxml"));
 
-    Scene scene = new Scene(root, 1510, 890);
+    Scene scene = new Scene(root, 1315, 890);
     primaryStage.setScene(scene);
     primaryStage.show();
   }
